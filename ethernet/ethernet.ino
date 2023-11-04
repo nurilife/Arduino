a66fdf3440ab766bb61f0fa20485b68acaca9e31
Youngho Park <allnurilife@gmail.com>
2023-11-04 19:17:43 +0900

    feat : 이더넷 파일 추가

---
+++ ethernet/ethernet.ino
@@ -0,0 +1,60 @@
+#include <EtherCard.h>
+
+#define STATIC 0  // set to 1 to disable DHCP (adjust myip/gwip values below)
+
+#if STATIC
+// ethernet interface ip address
+static byte myip[] = { 192,168,0,6 };
+// gateway ip address
+static byte gwip[] = { 192,168,0,1 };
+#endif
+
+// ethernet mac address - must be unique on your network
+static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
+
+byte Ethernet::buffer[700]; // tcp/ip send and receive buffer
+
+const char page[] PROGMEM =
+"HTTP/1.0 503 Service Unavailable\r\n"
+"Content-Type: text/html\r\n"
+"Retry-After: 600\r\n"
+"\r\n"
+"<html>"
+  "<head><title>"
+    "Hello from Arduino RJ Technology"
+  "</title></head>"
+  "<body>"
+    "<h3>This is an Arduino answering-RJ Technology</h3>"
+    "<p><em>"
+      "Have fun finding out more fun things to do  with this!<br />"
+      "Please try again and again."
+    "</em></p>"
+  "</body>"
+"</html>"
+;
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println("\n[backSoon]");
+  
+  if (ether.begin(sizeof Ethernet::buffer, mymac) == 0) 
+    Serial.println( "Failed to access Ethernet controller");
+#if STATIC
+  ether.staticSetup(myip, gwip);
+#else
+  if (!ether.dhcpSetup())
+    Serial.println("DHCP failed");
+#endif
+
+  ether.printIp("IP:  ", ether.myip);
+  ether.printIp("GW:  ", ether.gwip);  
+  ether.printIp("DNS: ", ether.dnsip);  
+}
+
+void loop(){
+  // wait for an incoming TCP packet, but ignore its contents
+  if (ether.packetLoop(ether.packetReceive())) {
+    memcpy_P(ether.tcpOffset(), page, sizeof page);
+    ether.httpServerReply(sizeof page - 1);
+  }
+}
