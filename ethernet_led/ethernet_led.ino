bfc417522f1e165c004c338b2110a53383add05e
nurilife <52094128+nurilife@users.noreply.github.com>
2023-11-05 18:37:10 +0900

    Update ethernet_led.ino
    프로그램에 대한 해석 (주석을 포함함)

--- ethernet_led/ethernet_led.ino
+++ ethernet_led/ethernet_led.ino
@@ -73,7 +73,9 @@ word len = ether.packetReceive();
 word pos = ether.packetLoop(len);
 
 // IF LED10=ON turn it ON
-if(strstr((char *)Ethernet::buffer + pos, "GET /?LED10=ON") != 0) {
+if(strstr((char *)Ethernet::buffer + pos, "GET /?LED10=ON") != 0) {   //Ethernet::buffer에 저장된 패킷을 수신하여 처리
+  //strstr() 함수는 "GET /?LED10=ON" 문자열이 Ethernet::buffer에 저장된 패킷에 포함되어 있는지 여부를 확인하는 함수
+  //(char *)Ethernet::buffer + pos: 수신된 패킷에서 "pos" 위치부터 시작하여 문자열을 가리킵니다.
 Serial.println("Received ON command");
 digitalWrite(ledPin10, HIGH);
 }
@@ -85,8 +87,11 @@ digitalWrite(ledPin10, LOW);
 }
 
 // show some data to the user
-memcpy_P(ether.tcpOffset(), page, sizeof page);
-ether.httpServerReply(sizeof page - 1);
+memcpy_P(ether.tcpOffset(), page, sizeof page);  //ether.tcpOffset() 함수를 호출하여 페이지 변수에 저장된 HTML 코드를 Ethernet::buffer에 복사
+   //memcpy_P() 함수는 프로그램 메모리에 저장된 데이터를 일반 메모리에 복사하는 역할
+   //ether.tcpOffset() 함수는 Ethernet::buffer 내에서 TCP 데이터가 시작되는 위치를 반환
+   //page 변수에는 웹 페이지의 HTML 코드가 저장  
+ether.httpServerReply(sizeof page - 1);  //ether.httpServerReply() 함수를 호출하여 복사된 HTML 코드를 사용자에게 전송
 }
 
 void blinkLed(){
